a59429c8f77c982252b271bf91188ae495f9dcbd
Bram Borggreve <borggreve@gmail.com>
2018-10-31 23:46:21 -0500

    fix issues with lesson 2

--- src/app/course/activities/lesson-data/lesson2.ts
+++ src/app/course/activities/lesson-data/lesson2.ts
@@ -125,8 +125,7 @@ const activity4: Activity = {
   ],
   solution: `this.button$
   .pipe(map(() => this.multiplyAmount))
-  .subscribe((amount) => this.increaseCounter('button', amount));
-`,
+  .subscribe((amount) => this.buttonCounter = this.buttonCounter + amount);`,
   steps: [
     {
       step: `Import <code>map</code> from <code>rxjs/operators</code>.`,
@@ -149,8 +148,8 @@ const activity4: Activity = {
       code: `.subscribe();`,
     },
     {
-      step: `Take the amount returned by the map function and pass it in the <code>increaseCounter()</code> method.`,
-      code: `.subscribe((amount) => this.increaseCounter('button', amount));`,
+      step: `Take the amount returned by the map function and add it to the current value of <code>this.buttonCounter</code>.`,
+      code: `.subscribe((amount) => this.buttonCounter = this.buttonCounter + amount);`,
     },
   ],
 };
@@ -202,12 +201,11 @@ const activity6: Activity = {
 this.button$
   .pipe(takeUntil(buttonStop))
   .subscribe(() => {
-    if (this.result + 1 === this.takeUntilAmount) {
+    if (this.buttonCounter + 1 === this.takeUntilAmount) {
       buttonStop.next();
     }
-    this.increaseCounter('button');
-  });
-`,
+    this.buttonCounter++;
+  });`,
   steps: [
     {
       step: `Import <code>takeUntil</code> from <code>rxjs/operators</code>.`,
@@ -236,7 +234,7 @@ this.button$
     {
       step: `Add an if-statement to determine when to trigger the stop.`,
       code: `() => {
-  if (this.counters.button + 1 === this.takeUntilAmount) { }
+  if (this.buttonCounter + 1 === this.takeUntilAmount) { }
 }`,
     },
     {
@@ -253,7 +251,7 @@ this.button$
   if (this.result + 1 === this.takeUntilAmount) {
     buttonStop.next();
   }
-  this.increaseCounter('button');
+  this.buttonCounter++;
 }`,
     },
   ],
--- src/app/course/activities/lesson2/activity1.component.ts
+++ src/app/course/activities/lesson2/activity1.component.ts
@@ -4,9 +4,8 @@ import { } from 'rxjs/operators';
 
 @Component({
   template: `
-    <button #button class="btn btn-primary">
-      Clicked {{buttonCounter}} times
-    </button>
+    <button #button class="btn btn-primary">Click</button>
+    <app-result [result]="{ buttonCounter: buttonCounter }"></app-result>
   `,
 })
 export class Activity1Component implements OnInit {
@@ -37,6 +36,6 @@ export class Activity1Component implements OnInit {
    * Solution for Activity
    */
   solution() {
-    
+
   }
 }
